009fe11666698d0a788c2ecfe1c27b13c9cc8fe4
Lucas Vieira <lucas.vieira.l@bairesdev.com>
2025-02-17 11:22:20 -0300

    WIP V1

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,32 @@
 import Image from "next/image";
+import { MdMailOutline } from "react-icons/md";
 
 export default function Footer() {
   return (
-    <div className="relative h-96">
+    <div className="relative aspect-auto flex flex-col gap-16 py-10">
       <Image
         src="/footer-image.png"
         alt="logo"
         fill
-        className="object-cover object-top"
+        className="object-cover object-top z-40"
       />
 
-      <div className="relative h-52  mx-auto top-5 aspect-[2351/977]">
+      <div className="relative h-20 md:h-52  mx-auto top-5 aspect-[2351/977]">
         <Image className="z-50" src="/footer-logo.png" alt="logo" fill />
       </div>
+      <div className="z-50 relative w-full flex items-center justify-center text-xs">
+        <FooterItem>
+          <MdMailOutline />
+          contato@gheorghiuvieira.com
+        </FooterItem>
+      </div>
     </div>
+  );
+}
+
+type FooterItemProps = React.PropsWithChildren;
+function FooterItem({ children }: FooterItemProps) {
+  return (
+    <p className="text-white/80 text-sm flex gap-2 items-center">{children}</p>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,11 +5,11 @@ import { HTMLAttributes } from "react";
 export default function Navbar() {
   return (
     <nav className="absolute top-0 flex w-screen py-4 px-6 items-center justify-between z-50 bg-black/70 text-white/80">
-      <div className="relative h-20 aspect-[53/13]">
+      <div className="relative h-16 md:h-20 aspect-[53/13]">
         <Image src="/M6.png" alt="header" fill />
       </div>
 
-      <section className="flex gap-2">
+      <section className=" gap-2 hidden md:flex">
         <NavbarItem>Home</NavbarItem>
         <NavbarItem>Equipe</NavbarItem>
         <NavbarItem>Contato</NavbarItem>
