6c7b2ab95b969fd8f03637e17fa90bcef4d02915
Lucas <silveira.lsv@gmail.com>
2025-06-27 13:05:07 -0300

    fix: use esm import for tailwindcss-animate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
 
 export default {
     darkMode: ["class"],
@@ -58,5 +59,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animatePlugin],
 } satisfies Config;
